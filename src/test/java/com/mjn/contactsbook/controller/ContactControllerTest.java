9832e5c4d37f10d49adbeafd4cfc5ad55ac729b5
Michael Nowak <michal.nowakb@gmail.com>
2021-03-24 16:20:03 +0000

    adding new test class for ContactController

---
+++ src/test/java/com/mjn/contactsbook/controller/ContactControllerTest.java
@@ -0,0 +1,28 @@
+package com.mjn.contactsbook.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContactControllerTest {
+
+    @Test
+    void getAllContacts() {
+    }
+
+    @Test
+    void getContactById() {
+    }
+
+    @Test
+    void createContact() {
+    }
+
+    @Test
+    void updateContact() {
+    }
+
+    @Test
+    void deleteContactById() {
+    }
+}
